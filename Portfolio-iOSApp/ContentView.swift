b71106c2efa2e6d37798235aef344448ac5a5d1f
Palatip Jantawong <palatipjant@outlook.com>
2023-10-17 18:58:39 +0700

    start

--- Portfolio-iOSApp/ContentView.swift
+++ Portfolio-iOSApp/ContentView.swift
@@ -6,16 +6,64 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.vertical){
+            ZStack{
+                Image("home_bg")
+                    .resizable()
+                    .scaledToFit()
+                VStack{
+                    Image("profile_circle")
+                        .padding()
+                    Text("Palatip Jantawong")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .padding()
+                    HStack{
+                        Button(action: {
+                            
+                        }, label: {
+                            FontIcon.text(.materialIcon(code: .apps),fontsize: 35)
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            FontIcon.text(.materialIcon(code: .apps),fontsize: 35)
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            FontIcon.text(.materialIcon(code: .apps),fontsize: 35)
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            FontIcon.text(.materialIcon(code: .apps),fontsize: 35)
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            FontIcon.text(.materialIcon(code: .apps),fontsize: 35)
+                        })
+                    }
+                    .padding(.bottom)
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Contact Me")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                    }).frame(width: 160, height: 45)
+                        .foregroundStyle(.white)
+                        .background(Color("lightred"))
+                        .clipShape(.capsule)
+                }.foregroundStyle(.white)
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
