d61cab5a129101ff5ff5a007181a2fa535428288
Palatip Jantawong <palatipjant@outlook.com>
2023-10-17 18:09:11 +0700

    Initial Commit

---
+++ Portfolio-iOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Portfolio-iOSApp
+//
+//  Created by Palatip Jantawong on 17/10/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Portfolio-iOSApp/Portfolio_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio_iOSAppApp.swift
+//  Portfolio-iOSApp
+//
+//  Created by Palatip Jantawong on 17/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
